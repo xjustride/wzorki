9824dfd86cedcc81604a3887cd608162757c030b
xjustride <remigiusz.krupa@microsoft.wsei.edu.pl>
2022-12-07 18:53:07 +0100

    Add project files.

---
+++ koloszad 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class TriangleProgram
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = n; j > i ; j--)
+            {
+                Console.Write("*");
+            }
+            for (int z = 0; z >= (i*2); z++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("\n");
+        }
+
+
+
+
+    }
+
+
+
+
+}
